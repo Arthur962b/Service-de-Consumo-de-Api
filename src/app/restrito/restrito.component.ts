78fde4c13c37ef2cb5439ef09a5c6cd227cd0e65
Arthur <arthurdeastora@gmail.com>
2025-09-14 14:53:19 -0300

    commit

---
+++ src/app/restrito/restrito.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
+
+@Component({
+  selector: 'app-restrito',
+  templateUrl: './restrito.component.html',
+  styleUrls: ['./restrito.component.css']
+})
+export class RestritoComponent {
+  private _router: any;
+  constructor(private_router: Router, private _loginService: LoginService) {}
+   
+
+    logout(){
+      localStorage.clear();
+      this._loginService.setMostraMenu(true);
+      this._router.navigate(['/login']);
+    }
+  }
